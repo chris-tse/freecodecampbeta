435836f3c28f005c876714818cf2c05db4f44f9a
Christopher Tse <christopher-tse@outlook.com>
2017-10-22 00:24:14 -0500

    Remove console logs

--- Basic Algorithm Scripting/rot13.js
+++ Basic Algorithm Scripting/rot13.js
@@ -1,18 +1,19 @@
 function rot13(str) { // LBH QVQ VG!
     // split into array of char codes
-    let codeArr = str.split('').map(char => {
-        return char.charCodeAt()
-    })
+    let codeArr = str.split('').map(char => char.charCodeAt())
 
     // loop through char codes and calculate original char codes
     let origCodeArr = codeArr.map( code => {
+
+        // do computation on char code if it is code of alphabet, else return with no computation
         if (code >= 65 && code <= 90) {
             let newCode = code - 13
 
-            if (newCode === 32 || newCode >= 65) {
+            // if subtract 13 doesn't go below 'A' (65) return
+            if (newCode >= 65) {
                 return newCode
             }
-
+            // else loop around from 'Z' (90)
             else {
                 return 90 - (65 - (newCode + 1))
             }
@@ -27,4 +28,4 @@ function rot13(str) { // LBH QVQ VG!
 
 // Change the inputs below to test
 //rot13("ABC XYZ");
-console.log(rot13("SERR PBQR PNZC"));
+console.log(rot13("SERR PBQR PNZC"))
