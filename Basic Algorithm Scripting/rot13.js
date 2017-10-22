025a822063964cbe862f4081412ab8add4e3dd51
Christopher Tse <christopher-tse@outlook.com>
2017-10-22 00:20:05 -0500

    Refactor using ES6

--- Basic Algorithm Scripting/rot13.js
+++ Basic Algorithm Scripting/rot13.js
@@ -1,26 +1,30 @@
 function rot13(str) { // LBH QVQ VG!
-    var codeArr = [];
-    for (var i = 0; i < str.length; i++) {
-        codeArr.push(str.charCodeAt(i))
-    }
-    console.log(str.split(''));
-    console.log(codeArr);
-    for (var i = 0; i < codeArr.length; i++) {
-        if(codeArr[i] >= 65 && codeArr[i] <= 90) {
-            var change = codeArr[i] - 13;
-            if(change >= 65)
-                codeArr[i] = change;
-            else
-                codeArr[i] = 90 - (65 - (change+1));
-        }
-    }
-    console.log(String.fromCharCode.apply(null, codeArr));
-    console.log(codeArr);
+    // split into array of char codes
+    let codeArr = str.split('').map(char => {
+        return char.charCodeAt()
+    })
 
-    return String.fromCharCode.apply(null, codeArr);
+    // loop through char codes and calculate original char codes
+    let origCodeArr = codeArr.map( code => {
+        if (code >= 65 && code <= 90) {
+            let newCode = code - 13
+
+            if (newCode === 32 || newCode >= 65) {
+                return newCode
+            }
+
+            else {
+                return 90 - (65 - (newCode + 1))
+            }
+        } else {
+            return code
+        }
+    })
+
+    return String.fromCharCode(...origCodeArr)
 
 }
 
 // Change the inputs below to test
 //rot13("ABC XYZ");
-rot13("SERR PBQR PNZC");
+console.log(rot13("SERR PBQR PNZC"));
